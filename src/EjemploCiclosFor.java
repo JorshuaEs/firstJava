3e2d3387f5a91c17a71cfaa0953834f0ded90c23
JorshuaEs <estradajorshua16@gmail.com>
2023-01-21 00:40:55 -0600

    first commit

---
+++ src/EjemploCiclosFor.java
@@ -0,0 +1,11 @@
+
+public class EjemploCiclosFor {
+	public static void main(String[] args) {
+		
+		
+		// variable || condicion || lo que se ejecuta
+		for(int contador = 0; contador <=10; contador++) {
+			System.out.println(contador);
+		}
+	}
+}
---
+++ src/TipoVariable.java
@@ -0,0 +1,18 @@
+
+public class TipoVariable {
+	public static void main(String[] args) {
+	
+		double variable1 = 230.89;
+		int variable1Entero = (int) variable1; //cast
+		//Este metodo imprime en consola
+		System.out.println(variable1Entero);
+		
+		long prueba = 1506436545431639876L;
+		System.out.println(prueba);
+		
+		short numeroPequeno = 13555;
+		
+		int resultado = (int) variable1 + variable1Entero;
+		System.out.println(resultado);
+	}
+}
